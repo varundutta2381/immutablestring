b11460a1990b5044665c52d269719c707c560adc
vdutt1 <vdutt1@NOIVDUTT142789.sapient.com>
2018-01-05 09:51:14 +0530

    Changes made

---
+++ src/immutablestring1/immutablestring2.java
@@ -0,0 +1,18 @@
+package immutablestring1;
+
+public class immutablestring2 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+		String s1 = "data";
+		String s2 = "base";
+		
+		s1 = s1+s2;
+		System.out.println(s1);
+
+	}
+
+}
